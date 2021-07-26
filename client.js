aa56ded5d7b3442fd1e73b61a9885955b0636e87
Dan St. Aubin <staubindj@gmail.com>
2021-07-26 16:30:41 -0500

    added random person selection and added them to dom

--- client.js
+++ client.js
@@ -4,10 +4,21 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('We are ready');
-    
-    $('body').append(`
-        <div>
-            <img src="https://github.com/${people[0].githubUsername}.png?size=250" alt="Profile image of Chris">
-        </div>
-    `)
+    for (let i = 0; i < people.length; i++) {
+        $('body').append(`
+            <div>
+                <img src="https://github.com/${people[i].githubUsername}.png?size=250" alt="Profile image of Chris">
+            </div>
+        `)
+    };
+    alert('Guess who is below!')
+    // grab random person
+    let targetPerson = people[randomNumber(0,people.length-1)].name;
+    // add to randomName tag
+    $('#randomName').append(targetPerson);
+    // add in listeners
+}
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
 }
